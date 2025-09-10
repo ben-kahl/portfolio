2e4ea4b67c7c97b92cfa1b4e57816b0d85864038
Ben Kahl <ben.kahl2002@gmail.com>
2025-09-10 15:17:56 -0700

    some more tweaks to the contacts page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,28 +1,29 @@
 'use client';
 import styles from "./page.module.css";
-import Link from "next/link";
 export default function Contact() {
   return (
     <div>
-      <h1>CONTACT</h1>
-      <div className={styles.contacts}>
-        <div>
-          <a href='ben.kahl2002@gmail.com'>
-            <div className={styles.emailIcon}></div>
-            ben.kahl2002@gmail.com
-          </a>
-        </div>
-        <div>
-          <a href='https://www.github.com/ben-kahl'>
-            <div className={styles.githubIcon}></div>
-            github.com/ben-kahl
-          </a>
-        </div>
-        <div>
-          <a href='https://www.linkedin.com/in/ben-kahl/'>
-            <div className={styles.linkedInIcon}></div>
-            linkedin.com/in/ben-kahl/
-          </a>
+      <div className={styles.textBody}>
+        <h1>CONTACT</h1>
+        <div className={styles.contacts}>
+          <div>
+            <a href='ben.kahl2002@gmail.com'>
+              <div className={styles.emailIcon}></div>
+              ben.kahl2002@gmail.com
+            </a>
+          </div>
+          <div>
+            <a href='https://www.github.com/ben-kahl'>
+              <div className={styles.githubIcon}></div>
+              github.com/ben-kahl
+            </a>
+          </div>
+          <div>
+            <a href='https://www.linkedin.com/in/ben-kahl/'>
+              <div className={styles.linkedInIcon}></div>
+              linkedin.com/in/ben-kahl/
+            </a>
+          </div>
         </div>
       </div>
       <h3>temp</h3>
